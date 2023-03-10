ad2555e3747720cea478e92e508336b610c60127
smilez-detcelfer <detcelfer.tech@pm.me>
2023-03-10 15:53:07 +0300

    gitignore to .idea

--- request_sender.py
+++ request_sender.py
@@ -1,20 +1,16 @@
 import requests
 import json
 
-
 BASE = 'http://127.0.0.1:5000/'
-# payload = {'username': 'smilez', 'secret_key': '123456'}
-# headers = {'accept': 'application/json'}
-# response = requests.post(BASE + 'api/getuserinfo', json=payload)
-# print(response.json())
+
 
 headers = {'accept': 'application/json'}
 # payload = {#'secret_key': 'jG10xrcBjaqfBWt-ZXC9yVmtooAQQgYzOgGF6yndDAsUVdJtm_14TQ'
-#            'username': 'smile1',
-#            'email': 'email1xxx@domain.xxx',
+#            'username': 'smile123',
+#            'email': 'email111xxx@domain.xxx',
 #            'password': '1234567'
 #            }
-#response = requests.post(BASE + 'api/generate_encrypted_secret', json=payload)
-payload = {'secret_key': 'jG10xrcBjaqfBWt-ZXC9yVmtooAQQgYzOgGF6yndDAsUVdJtm_14TQ'}
+# response = requests.post(BASE + 'api/generate_encrypted_secret', json=payload)
+payload = {'secret_key': 'Ttc0aGSzKeupM6ai6QjIkYwMftwBV2NZJ2XJ4ZZq0nmLqGPyCw8xIg'}
 response = requests.post(BASE + 'api/getuserinfo', json=payload)
 print(response.json())
--- src/utils/schedule_manager.py
+++ src/utils/schedule_manager.py
@@ -3,9 +3,9 @@ import os
 from src import scheduler
 from src.models import db, User
 
-@scheduler.task('interval', id='delete_old_users', seconds=15)
+@scheduler.task('interval', id='delete_old_users', days=10)
 def delete_old_users():
-    with scheduler.app.app_context():
+    with scheduler.app.app_context(): # coz function uses application parts
         current_time = datetime.utcnow()
         # set user deletion delta here:
         two_month_ago = current_time - timedelta(days=3)
@@ -15,9 +15,11 @@ def delete_old_users():
                                  f'{user.id},{user.username},{user.last_login}, {current_time}',
                                  user_for_delete))
         print(f'scheduled job "delete_old_users": {len(user_log_list)} users will be deleted')
-        User.query.filter(User.last_login < two_month_ago).delete()
-        db.session.commit()
         # change log file path here:
         logfile_path = os.path.join(os.getcwd(), "logs", "delete_old_users.log")
         with open(logfile_path, 'a', encoding='UTF-8') as file:
             file.writelines(line + '\n' for line in user_log_list)
+        # db cascade user delition
+        User.query.filter(User.last_login < two_month_ago).delete()
+        db.session.commit()
+
